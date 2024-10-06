db9826a4227857bb527af33affdd2d51607bd25d
Roodek <radmatyja@gmail.com>
2024-10-06 21:49:35 +0200

    init

---
+++ src/test/java/com/eco/ecosystem/EcosystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eco.ecosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcosystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
